588b3bc8650e8e951af8dbc3b4fbb64e3867bd80
vianismee <104370516+vianismee@users.noreply.github.com>
2025-02-26 22:25:39 +0700

    v0.0.2 feat: Live deploy on Vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import DarkMode from "@/components/ui/dark-light";
 import LocalTime from "@/components/ui/local-time";
 import Header from "@/components/Header";
+import Dashboard from "./../components/Dashboard/Dashboard";
 
 export default function Home() {
   return (
     <div className="@container">
       <div className="grid grid-row font-[family-name:var(--font-space-grotesk)]">
         <Header />
-        <h1 className="text-[40pt] font-bold">We Will Get</h1>
-        <LocalTime></LocalTime>
-        <DarkMode></DarkMode>
+        <Dashboard />
       </div>
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import LocalTime from "./ui/local-time";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="w-full border-zinc-700 border-b-[2px] py-6 px-[200px] flex justify-between items-center">
+      <div className="flex items-center gap-4 px-[18px] py-[13px] bg-primary-foreground rounded-2xl">
+        <span className="relative flex size-3">
+          <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-emerald-400 opacity-75"></span>
+          <span className="relative inline-flex size-3 rounded-full bg-green-500"></span>
+        </span>
+        <h1 className="font-bold">Work In Progress</h1>
+      </div>
+      <div className="flex items-center gap-4">
+        <h1 className="text-primary font-medium">Live Time</h1>
+        <span className="font-bold px-[18px] py-[13px] bg-primary-foreground rounded-xl flex w-[110px] justify-center border-secondary border">
+          <LocalTime />
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
