592259db5b00a52f642d95836f6cf017b3aa18e2
vianismee <104370516+vianismee@users.noreply.github.com>
2025-02-26 22:30:11 +0700

    v0.0.2(1) bugfix: erorr deploy 001

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import LocalTime from "@/components/ui/local-time";
+import React from "react";
 import Header from "@/components/Header";
 import Dashboard from "./../components/Dashboard/Dashboard";
 
