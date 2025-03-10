4f86447648189a5e260a4ffef3db72bd8aced287
vianismee <104370516+vianismee@users.noreply.github.com>
2025-03-10 22:23:28 +0700

    bug fix Button

--- src/components/StunningProject.tsx
+++ src/components/StunningProject.tsx
@@ -3,7 +3,6 @@
 import { motion } from "framer-motion";
 import { Rocket } from "lucide-react";
 import React from "react";
-import { Button } from "@/components/ui/button";
 import { stunningProject } from "@/data";
 
 export const StunningProject = () => {
